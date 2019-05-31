ece97049994172888801ead252d1a4c80d8066fe
Matt Kornfield <matt.kornfield@appian.com>
2019-05-31 01:05:14 -0400

    added rotating image

--- window.go
+++ window.go
@@ -4,6 +4,7 @@ import (
 	_ "image"
 	_ "image/png"
 	_ "os"
+	"time"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
@@ -15,6 +16,33 @@ func main() {
 }
 
 func run() {
+	win := loadWindow()
+
+	center := win.Bounds().Center()
+
+	win.Clear(colornames.Firebrick)
+
+	sprite := getSprite()
+
+	angle := 0.0
+	last := time.Now()
+	for !win.Closed() {
+		dt := time.Since(last).Seconds()
+		last = time.Now()
+		win.Clear(colornames.Firebrick)
+
+		mat := pixel.IM
+		mat = mat.ScaledXY(pixel.ZV, pixel.V(5, 5))
+		mat = mat.Rotated(pixel.ZV, angle)
+		angle += 3 * dt
+		mat = mat.Moved(center)
+		sprite.Draw(win, mat)
+
+		win.Update()
+	}
+}
+
+func loadWindow() *pixelgl.Window {
 	cfg := pixelgl.WindowConfig{
 		Title:  "Pixel Rocks!",
 		Bounds: pixel.R(0, 0, 1024, 768),
@@ -24,14 +52,16 @@ func run() {
 	if err != nil {
 		panic(err)
 	}
+	win.SetSmooth(true)
+	return win
+}
+
+func getSprite() *pixel.Sprite {
 	img, err := LoadPicture("crying.png")
 	if err != nil {
 		panic(err)
 	}
+
 	sprite := pixel.NewSprite(img, img.Bounds())
-	win.Clear(colornames.Silver)
-	sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
-	for !win.Closed() {
-		win.Update()
-	}
+	return sprite
 }
