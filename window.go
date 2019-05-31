2b109082c8f01f8c9962f31424dbd0e7a4dda23c
Matt Kornfield <matt.kornfield@appian.com>
2019-05-31 00:16:12 -0400

    add initial files

---
+++ window.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/faiface/pixel"
+	_ "github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
+
+func main() {
+	pixelgl.Run(run)
+}
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Pixel Rocks!",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+	win.Clear(colornames.Skyblue)
+	for !win.Closed() {
+		win.Update()
+		win.Clear(colornames.Silver)
+	}
+}
